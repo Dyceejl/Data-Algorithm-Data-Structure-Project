e37c96d12ae81ed4f60a070654af40130e46bef8
Duong Khanh Trang <trang.dk2823@gmail.com>
2024-05-06 23:06:04 +0700

    Game Interface Update

---
+++ src/GameDisplay/GameFrame.java
@@ -0,0 +1,31 @@
+package GameDisplay;
+
+import javax.swing.JFrame;
+
+import Model.LoadData;
+
+public class GameFrame extends JFrame {
+
+    private LoadData loadData;
+
+    private GamePanel gamePanel;
+
+    public GameFrame() {
+
+        loadData = new LoadData();
+
+        add(gamePanel = new GamePanel(9, 9, 10));
+
+        setIconImage(loadData.getListImage().get("title"));
+
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+    
+}
